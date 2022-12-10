8b903e928979f96f5f1203aac31ea0f858adc8d7
mohdhanif1234 <hanif.topia@gmail.com>
2022-12-10 20:57:10 +0530

    Encapsulation assignment submitted

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+      RWOnly objRWOnly = new RWOnly(); // creating object of RWOnly class
+      objRWOnly.setName("Hanif"); // setting the field 'name'
+      System.out.println(objRWOnly.getName());
+  }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
+
 public class RWOnly {
-    
+    private String name;
+    // getter function to get name
+    public String getName() {
+        return name;
+    }
+
+    // setter function to set name
+    public void setName(String name) {
+        this.name = name;
+    }
 }
